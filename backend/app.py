f13fea655540b0befda8ddc72ff5d3ef8d867273
James Madden <jamesbmadden@gmail.com>
2023-01-28 16:18:56 -0500

    Make sure it serves properly

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,18 @@
 # Import Flask
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, send_file
 
 # Initialize Flask app
 app = Flask(__name__)
 
-@app.route('/<path:path>')
-def index(path):
+@app.route('/')
+def index():
     print('Request for index page received')
-    return send_from_directory('../web/dist', path)
+    return send_file('../web/dist/index.html')
+
+@app.route('/assets/<path:path>')
+def assets(path):
+    print('Request for', path, 'recieved')
+    return send_from_directory('../web/dist/assets', path)
 
 
 if __name__ == '__main__':
