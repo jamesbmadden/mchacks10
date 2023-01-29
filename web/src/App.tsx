230fd569e25b35f357c130f30fd5f548538b3e47
James Madden <jamesbmadden@gmail.com>
2023-01-28 23:35:50 -0500

    Load component from API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import Calendar from './components/Calendar'
 import { Course } from './types/course'
+import ScheduleView from './components/ScheduleVIew'
 
 const TEST_SCHEDULE: Course[] = [
 
@@ -43,62 +44,33 @@ const TEST_SCHEDULE: Course[] = [
 ]
 
 function App() {
-  return (
-    <div className="suggested-schedule">
-      <nav className="nav-pane">
 
-        <h2 className="nav-h2-text">Rate my Professor Stats</h2>
+  const [ isLoaded, setIsLoaded ] = useState(false)
+  const [ schedule, setSchedule ] = useState([])
 
-        <div className="prof" style={{ background: '#f48fb1' }}>
-          <p className="prof-name">MATH 141</p>
-          <p className="prof-name">Sidney Trudeau</p>
-          <p className="prof-details">Quality: ⭐️⭐️⭐️⭐️⭐️</p>
-          <p className="prof-details">Difficulty: ⭐️⭐️⭐️⭐️⭐️</p>
-        </div>
+  useEffect(() => {
 
-        <div className="prof" style={{ background: '#81d4fa' }}>
-          <p className="prof-name">PHYS 142</p>
-          <p className="prof-name">Hong Guo</p>
-          <p className="prof-details">Quality: ⭐️⭐️⭐️⭐️⭐️</p>
-          <p className="prof-details">Difficulty: ⭐️⭐️⭐️⭐️⭐️</p>
-        </div>
+    fetch('/api/schedule/COMP,PHYS,MATH/208,142,141/True').then(response => response.json()).then(json => {
 
-        <div className="prof" style={{ background: '#ce93d8' }}>
-          <p className="prof-name">COMP 202</p>
-          <p className="prof-name">Comp Prof</p>
-          <p className="prof-details">Quality: ⭐️⭐️⭐️⭐️⭐️</p>
-          <p className="prof-details">Difficulty: ⭐️⭐️⭐️⭐️⭐️</p>
-        </div>
+      // here's the data! now we build the ideal schedule! 
+      let courses = [];
 
-        <div className="prof" style={{ background: '#aed581' }}>
-          <p className="prof-name">CHEM 120</p>
-          <p className="prof-name">Pallavi Sirjoosingh</p>
-          <p className="prof-details">Quality: ⭐️⭐️⭐️⭐️⭐️</p>
-          <p className="prof-details">Difficulty: ⭐️⭐️⭐️⭐️⭐️</p>
-        </div>
+      console.log(json);
 
-        <div className="prof" style={{ background: '#ffab91' }}>
-          <p className="prof-name">WCOM 206</p>
-          <p className="prof-name">Emilia Popova</p>
-          <p className="prof-details">Quality: ⭐️⭐️⭐️⭐️⭐️</p>
-          <p className="prof-details">Difficulty: ⭐️⭐️⭐️⭐️⭐️</p>
-        </div>
+      for (let i = 0; i < json.courses.length; i++) {
+        courses.push(json.courses[i][parseInt(json.record_holder[i]) - 1])
+      }
 
+      // @ts-ignore
+      setSchedule(courses)
+      setIsLoaded(true)
 
-      </nav>
+    });
 
-      <main className="content-pane">
+  }, [])
 
-        <h1>Class Schedule Builder</h1>
-        
-
-        <h2 className="term-text">Winter 2023</h2>
-
-        <Calendar startTime={8} endTime={22} schedule={TEST_SCHEDULE}></Calendar>
-
-      </main>
-    </div>
-  )
+  if (isLoaded) return <ScheduleView schedule={schedule}></ScheduleView>
+  else return <p>loading...</p>
 }
 
 export default App
