46abf3bb2520113415ea6f49c4fdb92782e7cd38
James Madden <jamesbmadden@gmail.com>
2023-01-29 11:04:11 -0500

    Allow input of your own courses

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './App.css'
 import { Course } from './types/course'
 import ScheduleView from './components/ScheduleView'
 import LoadingView from './components/LoadingView'
-import InputPage from './components/InputPage'
 
 function App() {
+
+  const [needsInput, SetNeedsInput] = useState(true);
 
   const [ isLoaded, setIsLoaded ] = useState(false)
   const [schedule, setSchedule] = useState([])
 
+  const [ courses, setCourses ] = useState([])
+
+  const [ courseCount, setCourseCount ] = useState(1)
+
+  const form = useRef(null)
+
   useEffect(() => {
 
-    fetch('/api/schedule/COMP,PHYS,MATH/208,142,141/True').then(response => response.json()).then(json => {
+    if (needsInput) return;
+
+    console.log(courses);
+
+    // get the course codes and departments out of the courses array
+    const departments = courses.map(course => course[0]);
+    const codes = courses.map(course => course[1]);
+
+    fetch(`/api/schedule/${departments.join(',')}/${codes.join(',')}/True`).then(response => response.json()).then(json => {
 
       // here's the data! now we build the ideal schedule! 
       let courses = [];
@@ -29,18 +44,55 @@ function App() {
 
     });
 
-  }, [])
+  }, [needsInput])
 
-  return (
+  if (needsInput) return (
+    <div className='page'>
+
+      <h1>Class Schedule Builder</h1>
+
+      <p>Input courses:</p>
+      <div className="form" ref={form}>
+        {/* Get however many inputs we need */}
+        {Array.from(Array(courseCount)).map((_, i) => {
+          return <input className="course-input" key={i} id={"course-input-" + i} placeholder="MATH 141"></input>
+        })}
+        <div className='input-buttons'>
+          <button className='input-addbutton' onClick={() => setCourseCount(courseCount + 1)}>+</button>
+          <button className='input-addbutton' onClick={() => setCourseCount(courseCount - 1)} disabled={courseCount === 1}>-</button>
+        </div>
+          
+      </div>
+      <button onClick={() => {
+
+        // get the courses from the form
+        // @ts-ignore
+        let enteredCourses = [];
+        // @ts-ignore
+        form.current.querySelectorAll('.course-input').forEach(input => {
+
+          const [department, code] = input.value.split(" ");
+
+          enteredCourses.push([department.toUpperCase(), code]);
+
+        });
+        
+        // @ts-ignore
+        setCourses(enteredCourses)
+
+        SetNeedsInput(false)
+      }}>Make Schedule!</button>
+
+      <p className='footer'>Made with ♥ by The Serpents</p>
+    </div>
+  );
+  else return (
     <>
-      <InputPage></InputPage>
-      {/* <LoadingView></LoadingView>
-      {isLoaded && <ScheduleView schedule={schedule}></ScheduleView>} */}
+      
+      <LoadingView></LoadingView>
+      {isLoaded && <ScheduleView schedule={schedule}></ScheduleView>}
     </>
   )
-
-  if (isLoaded) return 
-  else return 
 }
 
 export default App
