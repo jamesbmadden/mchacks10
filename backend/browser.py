e4c731e48a6f5542c57bb16992cc3a7ba61d8bcc
James Madden <jamesbmadden@gmail.com>
2023-01-28 20:21:12 -0500

    Add loading rate my professor score

--- backend/browser.py
+++ backend/browser.py
@@ -1,13 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--blink-settings=imagesEnabled=false")
 
 browser = webdriver.Chrome(options=chrome_options)
+
+# get the info about a prof on rate my professor
+def get_rmp_score(professor):
+  # open up the rmp homepage
+  browser.get('https://www.ratemyprofessors.com/')
+  # and enter McGill in to get started
+  browser.find_element(By.CLASS_NAME, 'Search__DebouncedSearchInput-sc-10lefvq-1').send_keys('McGill')
+  time.sleep(1)
+  browser.find_element(By.CLASS_NAME, 'Search__DebouncedSearchInput-sc-10lefvq-1').send_keys(Keys.ENTER)
+  time.sleep(1)
+  browser.find_element(By.CLASS_NAME, 'Search__DebouncedSearchInput-sc-10lefvq-1').send_keys(professor)
+  time.sleep(1)
+  browser.find_element(By.CLASS_NAME, 'Search__DebouncedSearchInput-sc-10lefvq-1').send_keys(Keys.ENTER)
+  
+  # wait until the new page has loaded
+  WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'RatingValue__Numerator-qw8sqy-2')))
+
+  # now grab the scores and return them :)
+  rating = float(browser.find_element(By.CLASS_NAME, 'RatingValue__Numerator-qw8sqy-2').get_property('innerText'))
+  difficulty = float(browser.find_elements(By.CLASS_NAME, 'FeedbackItem__FeedbackNumber-uof32n-1')[1].get_property('innerText'))
+
+  # now return both values!
+  return (rating, difficulty)
+
+print(get_rmp_score("Sidney Trudeau"))
 
 # get info about a class
 def get_course(department, code):
